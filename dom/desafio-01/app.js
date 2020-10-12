a2193e7d4ce8f9827de24b23aed96930fad970d8
gustcoder <gustavofaramos@gmail.com>
2020-10-12 20:50:20 -0300

    Sessao 2: aulas 10 a 18 + Desafio 01

---
+++ dom/desafio-01/app.js
@@ -0,0 +1,16 @@
+new Vue({
+    el: '#desafio',
+    data: {
+        nome: 'Gustavo Faria Ramos',
+        idade: 31,
+        myImg: 'https://s3.amazonaws.com/thinkific-import/220759/AaQOupKTMCZDEzzmIaSR_SO-LOGO-300.png' 
+    },
+    methods: {
+        novaIdade: function(){
+            return this.idade * 3
+        },
+        numRandom: function(){
+            return Math.random()
+        }
+    }    
+})
