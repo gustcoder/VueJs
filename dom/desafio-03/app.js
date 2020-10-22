ac7ed8cb570b2ee15dedcec8560041dc6138dee6
gustcoder <gustavofaramos@gmail.com>
2020-10-21 22:39:08 -0300

    Desafio 03

---
+++ dom/desafio-03/app.js
@@ -0,0 +1,23 @@
+new Vue({
+    el: '#desafio',
+    data: {
+        valor: 0
+    },
+    computed: {
+        resetValor () {
+            if (this.valor == 37) {
+                setTimeout(() => this.valor = 0, 5000)
+            }
+        },
+        resultado () {
+            this.resetValor
+            return this.valor != 37 ? 'Valor Diferente' : 'Valor Igual'
+        },
+        /* implementacao do curso... Esqueci do Watch!!! =/ */
+        watch: {
+            resultado () {
+                setTimeout(() => this.valor = 0, 5000)            
+            }
+        }
+    }
+});
