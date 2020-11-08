149587ff0c30e0f9cfe28235ccff53bcd4f11c66
gustcoder <gustavofaramos@gmail.com>
2020-11-08 19:24:36 -0300

    aulas de CSS + desafio04 de estilos

---
+++ dom/desafio-04/app.js
@@ -0,0 +1,41 @@
+new Vue({
+	el: '#desafio',
+	data: {
+		classeEfeito: 'destaque',
+		noClass: ['noClass1', 'noClass2'],
+		customClass: 'customClass1',
+		removerTransform: false,
+		estilo: 'width: auto; height: 20px; backgroundColor: black; color: white;',
+		progressColor: 'blue',
+		progressHeight: '20px',
+		progressWidth: 0
+	},
+	computed: {
+		customStyle() {
+			return this.customClass
+		},
+		progressBar() {
+			if (this.progressWidth == 100) {
+				alert('Carregamento concluido!')
+			} else {
+				return {
+					backgroundColor: this.progressColor,
+					height: this.progressHeight,
+					width: `${this.progressWidth}px`
+				}
+			}
+		}
+	},
+	methods: {
+		iniciarEfeito() {
+			setInterval(() => {
+				this.classeEfeito == 'encolher' ? this.classeEfeito = 'destaque' :  this.classeEfeito = 'encolher'
+			}, 2000)
+		},
+		iniciarProgresso() {
+			setInterval(() => {
+				this.progressWidth += 10
+			}, 2000)
+		}
+	}
+})
