9219f96cd635b5dc2168a6d742274c82539e01ea
gustcoder <gustavofaramos@gmail.com>
2020-12-08 22:30:34 -0300

    desafio componentizacao

---
+++ componente-desafio/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-app>
+    <v-menu/>
+    <v-toolbar/>
+    <v-content>
+      <v-carousel/>
+    </v-content>  
+    <v-footer/>
+  </v-app>
+</template>
+
+<script>
+import Toolbar from './components/Toolbar.vue'
+import Menu from './components/Menu.vue'
+import Carousel from './components/Carousel.vue'
+import Footer from './components/Footer'
+export default {
+  components: {
+    'v-menu': Menu,
+    'v-carousel': Carousel,
+    'v-footer': Footer,
+    'v-toolbar': Toolbar
+  },
+  data() {
+    return {
+    }
+  }
+}
+</script>
