5e5c37569e359fd4c9f94f4422c301572ffbc0fa
gustcoder <gustavofaramos@gmail.com>
2020-12-08 22:13:56 -0300

    desafio - componentizacao

---
+++ vue-cli/projeto-simples/src/components/Button.vue
@@ -0,0 +1,18 @@
+<template>
+  <button class='newButton'>Novo Botão</button>
+</template>
+
+<script>
+export default {
+    name: 'newButton'
+}
+</script>
+
+<style>
+    .newButton {
+        width: 80px;
+        height: 60px;
+        background-color: crimson;
+        color: #fff;
+    }
+</style>
---
+++ vue-cli/projeto-simples/src/components/Componente.vue
@@ -0,0 +1,47 @@
+<template>
+    <div id="comp" v-if="!hideComp">
+        <h2>{{ title }} - {{ contador }}</h2>
+        <button @click="addContador(2)">Aumentar</button>
+        <newButton/>
+    </div>
+</template>
+
+<script>
+
+import newButton from './Button'
+
+export default {
+    name: 'Comp',
+    components: {
+        newButton
+    },
+    data: function() {
+        return {
+            title: 'Meu primeiro componente VueJs!!!',
+            contador: 0,
+        }
+    },
+    methods: {
+        addContador(passo) {
+            this.contador += passo
+        }
+    },
+    computed: {
+        hideComp() {
+            return this.contador === 30
+        }
+    },
+    watch: {
+        contador() {
+            this.hideComp
+        }
+    }
+}
+</script>
+
+<style>
+    #comp {
+        background-color: coral;
+        color: #fff;
+    }
+</style>
