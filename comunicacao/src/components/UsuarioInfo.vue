486a4e82187d9a7e298a64c9a59ca7edce6f4f03
gustcoder <gustavofaramos@gmail.com>
2020-12-12 23:05:06 -0300

    comunicacao direta entre componentes pai x filho via props

---
+++ comunicacao/src/components/UsuarioInfo.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="componente">
+        <h2>As Informações de Usuário</h2>
+        <p>Vários detalhes...</p>
+        <p>Nome do usuário: {{ addSobrenome() }}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        nome: {
+            type: String,
+            required: true,
+            // default: 'Informe o nome'
+            // default: function () { // default tambem pode ser uma funcao
+            //     return 'Informe o nome'
+            // }
+        }
+    },
+    methods: {
+        addSobrenome () {
+            return this.nome + ' Sobrenome'
+        }
+    }
+}
+</script>
+
+<style scoped>
+    .componente {
+        flex: 1;
+        background-color: #ec485f;
+        color: #fff;
+    }
+</style>
