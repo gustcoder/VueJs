890843977def88ea553769bf8766478156939f6a
gustcoder <gustavofaramos@gmail.com>
2020-12-23 22:56:07 -0300

    componentes avancados

---
+++ componentes-avancados/citacoes/src/components/Citacao.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="citacao">
+        {{ texto }}
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        texto: {
+            type: Array
+        }
+    }
+}
+</script>
+
+<style scoped>
+    .citacao {
+        border: 1px solid #DDD;
+        background-color: rgba(0, 0, 0, .1);
+        padding: 25px;
+        max-width: 450px;
+    }
+</style>
---
+++ componentes-avancados/citacoes/src/components/Citacoes.vue
@@ -0,0 +1,54 @@
+<template>
+    <div class="citacoes">
+        <span>
+            <button @click="numero--">&lt;</button>
+            <button @click="numero++">&gt;</button>
+        </span>
+        <Citacao :texto="citacoes[indice].texto" />
+    </div>
+</template>
+
+<script>
+import Citacao from "./Citacao.vue"
+
+export default {
+    components: {
+        Citacao
+    },
+    data() {
+        return {
+            numero: 0,
+            citacoes: [{
+                fonte: 'Jornal do Empreendedor',
+                texto: 'Lembre-se sempre que você é absolutamente único. Assim como todos os outros.',
+                autor: 'Margaret Mead'
+            }, { 
+                fonte: 'Frases de Mãe',
+                texto: 'Isso não é um quarto, é um chiqueiro.',
+                autor: 'Roberta'
+            }, {
+                fonte: 'Frases de Pai',
+                texto: 'Vou contar até 3! 1, 2, 2...',
+                autor: 'Gustavo'
+            }]
+        }
+    },
+    created() {
+        // console.log(this.citacoes[0])
+    },
+    computed: {
+        indice() {
+            // console.log(Math.abs(this.numero % 3))
+            return Math.abs(this.numero % 3)
+        }
+    }
+}
+</script>
+
+<style scoped>
+    .citacoes {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+    }
+</style>
